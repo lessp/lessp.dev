f22df4c28d05516b5164202595a8e845972fbdeb
Tom Ekander <ekander.tom@gmail.com>
2019-09-21 19:14:22 +0200

    chore: formatting

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -5,15 +5,21 @@ export const H1 = ({ children, additionalClassnames = "" }) => (
 )
 
 export const H2 = ({ children, additionalClassnames = "" }) => (
-  <h2 className={`text-sm font-semibold text-gray-500 leading-loose tracking-wide uppercase ${additionalClassnames}`}>{children}</h2>
+  <h2
+    className={`text-sm font-semibold text-gray-500 leading-loose tracking-wide uppercase ${additionalClassnames}`}
+  >
+    {children}
+  </h2>
 )
 
 export const H3 = ({ children, additionalClassnames = "" }) => (
-  <h3 className={`text-lg font-semibold mb-2 inline-block ${additionalClassnames}`}>{children}</h3>
+  <h3
+    className={`text-lg font-semibold mb-2 inline-block ${additionalClassnames}`}
+  >
+    {children}
+  </h3>
 )
 
 export const Strong = ({ children, additionalClassnames = "" }) => (
   <span className={`font-bold ${additionalClassnames}`}>{children}</span>
 )
-
-
