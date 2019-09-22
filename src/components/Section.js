3c8d2babbf1733cf2d268e30c00ef7b18a121c62
Tom Ekander <ekander.tom@gmail.com>
2019-09-22 10:54:45 +0200

    fix(css): section title line-up

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,7 @@ import React from "react"
 import { H2 } from "./Typography"
 
 const Section = ({ title, children }) => (
-  <section className="flex flex-col items-center mb-8 px-8 md:px-0 md:flex-row md:items-start">
+  <section className="flex flex-col items-start mb-8 px-8 md:px-0 md:flex-row">
     <H2 additionalClassnames="w-full text-left md:w-1/5 md:mr-8 md:text-right">
       {title}
     </H2>
