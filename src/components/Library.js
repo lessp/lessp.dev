7c575ce0141e0788cc969856364ae82d955e84cd
Tom Ekander <ekander.tom@gmail.com>
2019-12-03 10:29:42 +0100

    feat: add github stars to libraries

--- src/components/Library.js
+++ src/components/Library.js
@@ -13,31 +13,46 @@ const Library = ({ name, description, npm = true }) => (
       <H3>{name}</H3>
     </a>
     <p>{description}</p>
-    {npm && (
-      <div className="flex items-center mt-3">
-        <a
-          href={`https://npm.im/${name}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            className="mr-2"
-            alt={`npm downloads badge for ${name}`}
-            src={`https://img.shields.io/npm/dt/${name}?style=flat-square`}
-          />
-        </a>
-        <a
-          href={`https://npm.im/${name}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            alt={`npm version badge for ${name}`}
-            src={`https://img.shields.io/npm/v/${name}?style=flat-square`}
-          />
-        </a>
-      </div>
-    )}
+    <div className="flex items-center mt-3">
+      {npm && (
+        <>
+          <a
+            href={`https://npm.im/${name}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="mr-2"
+              alt={`npm downloads badge for ${name}`}
+              src={`https://img.shields.io/npm/dt/${name}?style=flat-square`}
+            />
+          </a>
+          <a
+            href={`https://npm.im/${name}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="mr-2"
+              alt={`npm version badge for ${name}`}
+              src={`https://img.shields.io/npm/v/${name}?style=flat-square`}
+            />
+          </a>
+        </>
+      )}
+      <a
+        href={`https://github.com/lessp/${name}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="mr-2"
+          alt={`Github stars badge for ${name}`}
+          src={`https://img.shields.io/github/stars/lessp/${name}.svg?style=social&label=Star&maxAge=2592000)
+`}
+        />
+      </a>
+    </div>
   </div>
 )
 
